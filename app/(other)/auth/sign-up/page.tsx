5ebfff59e12c8196eaab3cd17fa963c909216f98
Khaan25 <ziarzai@hotmail.com>
2025-06-09 19:11:18 +0500

    fix: build

--- app/(other)/auth/sign-up/page.tsx
+++ app/(other)/auth/sign-up/page.tsx
@@ -2,7 +2,11 @@ import AuthWrapper from '@/features/auth/components/auth-wrapper'
 import { SignUpForm } from '@/features/auth/components/forms/sign-up-form'
 import { MessageParams } from '@/types'
 
-export default async function Page({ searchParams }: { searchParams: MessageParams }) {
+type SignUpPageProps = {
+  searchParams: Promise<MessageParams>
+}
+
+export default async function Page({ searchParams }: SignUpPageProps) {
   const params = await searchParams
 
   return (
